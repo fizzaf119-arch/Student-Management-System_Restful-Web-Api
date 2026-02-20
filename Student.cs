d56d3c1a91e6a79d1afc72e1f5e08bc53f56d87f
Fizza Fatima <fizzaf119@gmail.com>
2026-02-21 01:20:50 +0500

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HW1_SCD.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        [Required(ErrorMessage ="Name is Required")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage ="Email is Required")]
+        [EmailAddress(ErrorMessage ="Invalid Email Format")]
+        public string Email { get; set; }
+
+
+
+        [Required(ErrorMessage = "Department is Required")]
+
+        public string Department { get; set; }
+
+
+
+        [Range(0,4,ErrorMessage = "CGPA must be between 0 and 4")]
+        public double cgpa { get; set; }
+
+
+
+    }
+}
